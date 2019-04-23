472c49591a674816c4ae2b65e407be09a3697a97
SamuFigueirido <samuelfigueirido@hotmail.com>
2019-04-23 17:04:38 +0200

    feat(Task-8): management of the volume and velocity of the video

--- Exercise11/scripts.js
+++ Exercise11/scripts.js
@@ -4,7 +4,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
     if (video.paused) {
@@ -24,9 +24,15 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
